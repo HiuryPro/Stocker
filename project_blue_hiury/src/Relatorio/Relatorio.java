f34f4d1751027e9831b72550bd9671a4a6515dce
HiuryPro <hiurylucas@unipam.edu.br>
2022-06-05 15:28:26 -0300

    Relatoria advancing I

--- project_blue_hiury/src/Relatorio/Relatorio.java
+++ project_blue_hiury/src/Relatorio/Relatorio.java
@@ -37,20 +37,31 @@ import net.sf.jasperreports.view.JasperViewer;
  */
 public class Relatorio {
 
-    Connection con, con1, con2;
+    Connection con, con1, con2, conexao;
     PreparedStatement pst;
+    ResultSet rs = null;
+    ResultSet rs1 = null;
+    Statement st;
+
+    public String data;
+    public String data2;
+
+    public Relatorio() {
+
+        conexao = ModuloConexao.conector();
+    }
 
     public void criaRelatorio(JTextField deData, JTextField ateData) {
         try {
+            data = deData.getText();
+            data2 = ateData.getText();
 
-            String url = "jdbc:mysql://localhost:3306/stocker";
-            String user = "root";
-            String password = "";
             Class.forName("com.mysql.jdbc.Driver");
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/stocker", "root", "");
 
             JasperDesign jDesign = JRXmlLoader.load("C:\\Users\\Hiury\\Documents\\Git-Projeto\\Stocker\\project_blue_hiury\\src\\Relatorio\\report1.jrxml");
             String sql = "SELECT * FROM produto_compra where (data_entrada BETWEEN STR_TO_DATE('" + deData.getText() + " ', \"%d/%m/%Y\") AND STR_TO_DATE('" + ateData.getText() + " ', \"%d/%m/%Y\"))"; // or  (data BETWEEN STR_TO_DATE('" + deData.getText() + " ', \"%d/%m/%Y\") AND STR_TO_DATE('" + ateData.getText() + " ', \"%d/%m/%Y\"))";
+            distinguiR(1);
 
             JRDesignQuery updateQuery = new JRDesignQuery();
             updateQuery.setText(sql);
@@ -73,15 +84,15 @@ public class Relatorio {
 
     public void criaRelatorio2(JTextField deData, JTextField ateData) {
         try {
+            data = deData.getText();
+            data2 = ateData.getText();
 
-            String url = "jdbc:mysql://localhost:3306/stocker";
-            String user = "root";
-            String password = "";
             Class.forName("com.mysql.jdbc.Driver");
             con1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/stocker", "root", "");
 
             JasperDesign jDesign = JRXmlLoader.load("C:\\Users\\Hiury\\Documents\\Git-Projeto\\Stocker\\project_blue_hiury\\src\\Relatorio\\relatorio.jrxml");
             String sql = "SELECT * FROM produto_venda where (data_saida BETWEEN STR_TO_DATE('" + deData.getText() + " ', \"%d/%m/%Y\") AND STR_TO_DATE('" + ateData.getText() + " ', \"%d/%m/%Y\"))"; // or  (data BETWEEN STR_TO_DATE('" + deData.getText() + " ', \"%d/%m/%Y\") AND STR_TO_DATE('" + ateData.getText() + " ', \"%d/%m/%Y\"))";
+            distinguiR(0);
 
             JRDesignQuery updateQuery = new JRDesignQuery();
             updateQuery.setText(sql);
@@ -104,10 +115,10 @@ public class Relatorio {
 
     public void criaRelatorio3(JTextField deData, JTextField ateData) {
         try {
-
-            String url = "jdbc:mysql://localhost:3306/stocker";
-            String user = "root";
-            String password = "";
+            data = deData.getText();
+            data2 = ateData.getText();
+            
+            
             Class.forName("com.mysql.jdbc.Driver");
             con2 = DriverManager.getConnection("jdbc:mysql://localhost:3306/stocker", "root", "");
 
@@ -131,13 +142,11 @@ public class Relatorio {
                 java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
                 utilDate2 = format.parse(ateData.getText());
                 java.sql.Date sqlDate2 = new java.sql.Date(utilDate2.getTime());
-                
-                
-                hm.put("query", sqlDate);
-                hm.put("query2", sqlDate2);
-                
-                
-                JasperPrint jPrint = JasperFillManager.fillReport(jReport, null, con2);
+
+                hm.put("query", data);
+                hm.put("query2", data2);
+
+                JasperPrint jPrint = JasperFillManager.fillReport(jReport, hm, con2);
                 JasperViewer.viewReport(jPrint, false);
 
             } catch (ParseException ex) {
@@ -154,4 +163,74 @@ public class Relatorio {
         }
     }
 
+    public void distinguiR(int id) {
+
+        String sql = "SELECT nome_produto FROM produto_venda";
+        try {
+
+            st = conexao.createStatement();
+            rs = st.executeQuery(sql);
+
+            while (rs.next()) {
+                pegaRel(id, rs.getString("nome_produto"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Relatorio.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void pegaRel(int id, String nomeP) {
+
+        if (id == 0) {
+
+            String sql = "SELECT SUM(quantidade), SUM(total), nome_produto FROM produto_venda where (data_saida BETWEEN STR_TO_DATE('" + data + " ', \"%d/%m/%Y\") AND STR_TO_DATE('" + data2 + " ', \"%d/%m/%Y\")) and nome_produto = '" + nomeP + "'";
+            try {
+
+                st = conexao.createStatement();
+                rs1 = st.executeQuery(sql);
+
+                while (rs1.next()) {
+                    relatoriototal(rs1.getString("nome_produto"), rs1.getInt("SUM(quantidade)"), rs1.getFloat("SUM(total)"));
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(Relatorio.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else if (id == 1) {
+
+            String sql = "SELECT SUM(quantidadeC), SUM(totalC), produtoC FROM produto_compra where (data_entrada BETWEEN STR_TO_DATE('" + data + " ', \"%d/%m/%Y\") AND STR_TO_DATE('" + data2 + " ', \"%d/%m/%Y\")) and produtoC = '" + nomeP + "'";
+            try {
+
+                st = conexao.createStatement();
+                rs1 = st.executeQuery(sql);
+
+                while (rs1.next()) {
+                    relatoriototal(rs1.getString("produtoC"), rs1.getInt("SUM(quantidadeC)"), rs1.getFloat("SUM(totalC)"));
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(Relatorio.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+    }
+
+    public void relatoriototal(String nomeP, int qtdTotal, float valorTotal) {
+
+        int count, qtd2;
+
+        String sql = "UPDATE relatoriototal SET qtd_total = ? , preco_total = ? WHERE nome_produto = '" + nomeP + "'";
+        try {
+            
+            
+            
+            pst = conexao.prepareStatement(sql);
+            pst.setInt(1, qtdTotal);
+            pst.setFloat(2, valorTotal);
+
+            pst.executeUpdate();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+
+    }
 }
